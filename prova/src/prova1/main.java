3df9fb23a456c7b192038cf5002eec2f0e2e19a8
amiyasarkaz <marccunillera2006@gmail.com>
2025-01-31 17:22:01 +0100

    Error retirar
    Quan vols retirar una cantita negativa salta

--- prova/src/prova1/main.java
+++ prova/src/prova1/main.java
@@ -10,7 +10,7 @@ public class main {
 	    myAccount = new Account("Flor Martinez", "1000-1234-56-123456789", 2500);
 	     
 	    try {
-	    	myAccount.withdrawAmount(2300);
+	    	myAccount.withdrawAmount(-2300);
 	    } catch(Exception e){
         	System.err.println(e.getMessage());
             System.out.println("Error al retirar");
